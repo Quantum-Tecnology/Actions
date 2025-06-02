251b8afa2c1cea88857f188bf0d8ff77c37ff46b
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-06-02 09:53:11 -0300

    first commit

---
+++ src/Providers/ActionServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Quantumtecnology\Actions\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class ActionServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
